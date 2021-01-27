e7d4b0c2a721f4b643d8be39eb247be29427a189
Lynch <E.Lynch@kostal.com>
2021-01-27 23:49:34 +0000

    Week2

--- Code/Week1/swordfish.py
+++ Code/Week1/swordfish.py
@@ -3,7 +3,7 @@ while True:
     name = input()
     if name != 'Enda':
         continue
-    print ('Hi Enda. What is the Password? (is it a fish?'))
+    print ('Hi Enda. What is the Password? (is it a fish?)')
     password = input()
     if password == 'swordfish':
         break
