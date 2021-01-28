98db1cd64e7d95b1ebb1cc21a00e07e851cabd8a
Lynch <E.Lynch@kostal.com>
2021-01-28 00:33:16 +0000

    lecture code week 2 28_01_2021

---
+++ Code/Week2/Lecture/addone.py
@@ -0,0 +1,6 @@
+#Author: Enda Lynch
+# Read in a number and add 1
+
+number = (input ('Please Enter a number:'))
+newnumber = int(number) + 1
+print ('{} plus 1 is {}' .format (number, newnumber))
---
+++ Code/Week2/Lecture/helloname.py
@@ -0,0 +1,13 @@
+# Author: Enda Lynch
+# Use a variable to greet user
+
+name = 'Enda'
+print ('Hello ' + name)
+
+# this wont work
+
+age = 34
+#print ('Yor age is ' + age)
+
+#print ('your age is ' + str(age) )
+print ( 'Hello {} \t Your age is {}' .format(name, age))
