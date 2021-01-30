8dab80b87c5620efad74886ede7e73a5c7698075
Lynch <E.Lynch@kostal.com>
2021-01-30 18:59:23 +0000

    Week02

--- Code/Week2/work.py
+++
@@ -1,7 +0,0 @@
-# Input weight (kg) and height (cm)
-Number1 = int (input('Enter First number: '))   
-Number2 = int (input('mutiplyed by: '))   
-
-Number3 = (Number1*Number2)
-
-print ('Your answer is ', (Number3))
