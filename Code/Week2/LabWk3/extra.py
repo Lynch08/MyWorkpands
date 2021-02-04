52835a1d22da72ac3863978493ce0a1eb959e5ac
Lynch <E.Lynch@kostal.com>
2021-02-04 01:18:33 +0000

    Week03

---
+++ Code/Week2/LabWk3/extra.py
@@ -0,0 +1,8 @@
+#Extra question
+
+#FIX:
+#message = 'I have eaten ' + 99 + ' burritos.'
+#print (message)
+
+message = 'I have eaten ' + str('99') + ' burritos.'
+print (message)
---
+++ Code/Week2/LabWk3/tabAndNextLine.py
@@ -0,0 +1,6 @@
+# Simple program creating and outputting a string
+# Author: Andrew Beatty
+
+#\t is tab - \n is next line
+message = 'Enda said\t"hi"\nI said\t\t\t\t"bye"'
+print (message)
